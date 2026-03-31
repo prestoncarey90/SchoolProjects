eb8254e916ccbad21fef27d32ba94bf31d8f8621
Preston Leslie <prestoncarey90@gmail.com>
2026-03-31 02:13:39 +0000

    Corrected test SQL code.

--- models/assignment_work/staging/test_311_sample.sql
+++ models/assignment_work/staging/test_311_sample.sql
@@ -4,5 +4,5 @@
      created_date,
      complaint_type,
      borough
- FROM {{ source('raw_db1', 'source_dot_service_requests_history') }}
+ FROM {{ source('raw', 'source_dot_service_requests_history') }}
  LIMIT 10
